4bb2b96c87b966ec98a2b61f4b8133bca0736391
sofiav19 <svitorica.ieu2022@student.ie.edu>
2024-10-03 13:40:50 +0200

    Added Country

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -26,7 +26,8 @@ def skull():
 def create_account():
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = request.json['country']
+    account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -63,5 +64,6 @@ def format_account(account):
         'balance': account.balance,
         'currency': account.currency,
         'status': account.status,
-        'created_at': account.created_at
+        'created_at': account.created_at,
+        'country': account.country
     }
