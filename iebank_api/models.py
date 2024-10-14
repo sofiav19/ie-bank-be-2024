67eb8e30fa469b6215d731c931dbb77670ed6af1
sofiav19 <svitorica.ieu2022@student.ie.edu>
2024-10-14 09:51:22 +0200

    added capitalize method

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -3,15 +3,16 @@ from datetime import datetime
 import string, random
 from sqlalchemy import func
 
+# add country field to the account model
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     account_number = db.Column(db.String(20), nullable=False, unique=True)
     balance = db.Column(db.Float, nullable=False, default = 0.0)
     currency = db.Column(db.String(1), nullable=False, default="€")
+    country = db.Column(db.String(32),nullable=False, default="Spain")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
-    country = db.Column(db.String(32),nullable=False, default="Spain")
 
     def __repr__(self):
         return '<Event %r>' % self.account_number
@@ -22,4 +23,4 @@ class Account(db.Model):
         self.currency = currency
         self.balance = 0.0
         self.status = "Active"
-        self.country = country
+        self.country = country.capitalize() # Capitalize the country name
